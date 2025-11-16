d69da4cbc4fa9b67e732102bf8dbc126811af9b9
SufiyaanRajput <sufiyaan@peoplegrove.com>
2025-11-16 11:59:39 +0530

    Stop tracking .docusaurus

--- repos/docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/peoplegrove/Repos/open-source/storeways/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/peoplegrove/Repos/open-source/storeways/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@17.0.89_react-dom@18.3.1_react@18.3.1__react@18.3.1_typescript@5.9.3/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/peoplegrove/Repos/open-source/storeways/node_modules/.pnpm/@docusaurus+theme-classic@3.9.2_@types+react@17.0.89_react-dom@18.3.1_react@18.3.1__react@18.3.1_typescript@5.9.3/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/peoplegrove/Repos/open-source/storeways/repos/docs/src/css/custom.css"),
-];
